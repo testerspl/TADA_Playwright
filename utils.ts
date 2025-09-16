a9f0c9ec43730d09fb5affd8de358cab2dc982a0
Bartlomiej Roszczypala <gucu112@hotmail.com>
2025-09-16 14:51:38 +0200

    Check input type attribute when setting password secretly

--- utils.ts
+++ utils.ts
@@ -1,3 +1,7 @@
 export function setPasswordSecretly(input: HTMLInputElement, password: string) {
+    if (input.getAttribute('type') !== 'password') {
+        throw new Error('Input is not a password field!');
+    }
+
     input.value = password;
 }
